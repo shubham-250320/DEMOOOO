72ed4d4faf6c405497b159946c621bfc13f1e252
Shubham Kumar Soni <sudhanshusoni227@gmail.com>
2026-05-26 11:17:52 +0530

    Fixed package configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,17 @@ import globals from "globals";
 export default [
   {
     files: ["**/*.js"],
+
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
         ...globals.jest
       }
+    },
+
+    rules: {
+      semi: ["error", "always"]
     }
   }
 ];
